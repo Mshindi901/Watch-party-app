baea030e8947e0c55f5381e155b39200e0f7af59
Mshindi901 <emmanuelmshindi68@gmail.com>
2026-02-11 16:49:19 +0300

    Had to add socket io setup

--- Server.js
+++ Server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -13,11 +15,42 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const httpServer = http.createServer(app);
+//Socket.io setup
+const io = new Server(httpServer, {
+    cors: {origin: '*',}
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected to Socket Id: ', socket.id);
+
+    socket.on('joinRoom', (roomId) => {
+        socket.join(roomId);
+        console.log(`Socket ${socket.id} joined room ${roomId}`);
+    });
+});
+
+socket.on('Play', (data) => {
+    socket.to(data.roomId).emit('Play', data);
+});
+
+socket.on('Pause', (data) => {
+    socket.to(data.roomId).emit('Pause', data);
+});
+
+socket.on('Seek', (data) => {
+    socket.to(data.roomId).emit('Seek', data);
+});
+
+socket.on('disconnect', () => {
+    console.log('A user disconnected from Socket Id: ', socket.id);
+});
+
 app.use('/auth', authRoutes);
 app.use('/room', roomRoutes);
 app.use('/participant', participantRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
